7f3bc577e653b0e7758632df30074f0ba2f2dc75
David Andersson <lejonmanen@gmail.com>
2025-03-13 09:37:58 +0100

    Event

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,39 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './App.css'
 
-const App = () => (
-	<>
-	<header>
-		<h1> Demo React </h1>
-	</header>
-	<main>
-		<button> Klicka här </button>
-		<p> Inget har hänt </p>
-	</main>
-	</>
-)
+const App = () => {
+	// State-variabler
+	// const temp = useState(0)
+	// const clicks = temp[0]
+	// const setClicks = temp[1]
+	const [clicks, setClicks] = useState(0)
+
+	// Eventuella events
+	const handleClick = () => {
+		setClicks(clicks + 1)  // motsvarar:  clicks = clicks + 1
+	}
+
+	// Eventuella variabler för JSX
+	let message
+	if( clicks > 0 ) {
+		message = clicks + ' klick'
+	} else {
+		message = 'Inget har hänt'
+	}
+	// Alternativ metod (med conditional operator)
+	message = clicks > 0 ? clicks + ' klick' : 'Inget har hänt'
+
+	return (
+		<>
+		<header>
+			<h1> Demo React </h1>
+		</header>
+		<main>
+			<button onClick={handleClick}> Klicka här </button>
+			<p> {message} </p>
+		</main>
+		</>
+	)
+}
 
 export default App
