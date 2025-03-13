92ace03f2cf01a9ffdd389f4afa1686a6ab50c40
David Andersson <lejonmanen@gmail.com>
2025-03-13 09:19:16 +0100

    Initial files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import './App.css'
+
+const App = () => (
+	<>
+	<header>
+		<h1> Demo React </h1>
+	</header>
+	<main>
+		<button> Klicka här </button>
+		<p> Inget har hänt </p>
+	</main>
+	</>
+)
+
+export default App
